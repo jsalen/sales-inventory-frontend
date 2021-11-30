9228bbf1ab802677fd85888e9b2279736ccaddb8
Joseph Salen <jsalen.91@gmail.com>
2021-11-30 15:26:55 -0400

    add reusable prop to placeholder

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -2,10 +2,10 @@ import { MdSearch } from 'react-icons/md'
 
 import { Form, Input, SearchButton } from './styles'
 
-export const SearchBar = () => {
+export const SearchBar = ({ text }) => {
   return (
     <Form>
-      <Input type='text' placeholder='Buscar...' />
+      <Input type='text' placeholder={text} />
       <SearchButton type='submit'>
         <MdSearch />
       </SearchButton>
