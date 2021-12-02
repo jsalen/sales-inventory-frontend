298f2358116b7f7251846241fcc2bae4e7818182
Joseph Salen <jsalen.91@gmail.com>
2021-12-02 19:34:37 -0400

    change layout acording to new navbar layout

--- src/Pages/Sales/styles.js
+++ src/Pages/Sales/styles.js
@@ -2,7 +2,6 @@ import styled from 'styled-components'
 
 export const Container = styled.section`
   width: 100%;
-  height: auto;
   display: grid;
-  grid-template-columns: 1fr 320px;
+  grid-template-columns: 75% 1fr;
 `
