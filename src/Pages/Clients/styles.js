790d75ecdbc885857f96f7511345b5f06d31c5a6
Joseph Salen <jsalen.91@gmail.com>
2021-12-13 15:37:15 -0400

    fix max-width

--- src/Pages/Clients/styles.js
+++ src/Pages/Clients/styles.js
@@ -2,8 +2,10 @@ import styled from 'styled-components'
 
 export const Container = styled.section`
   width: 100%;
+  max-width: 1280px;
   height: calc(100vh - 45px);
   padding: 24px;
+  margin: 0 auto;
   display: grid;
   grid-template-rows: 120px 1fr;
   grid-gap: 12px;
