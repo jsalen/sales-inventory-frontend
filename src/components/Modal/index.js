cb306abfc9bca6002cea41a8a6a878927173cf78
Joseph Salen <jsalen.91@gmail.com>
2021-12-15 19:32:56 -0400

    remove onClick

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -3,10 +3,8 @@ import ReactDOM from 'react-dom'
 import { Container } from './styles'
 
 export const Modal = ({ children, setOpenModal }) => {
-  const handleClick = () => setOpenModal((prevState) => !prevState)
-
   return ReactDOM.createPortal(
-    <Container onClick={handleClick}>{children}</Container>,
+    <Container>{children}</Container>,
     document.getElementById('modal')
   )
 }
