f457876430f972d06a869d39ca3170b676c6c964
Joseph Salen <jsalen.91@gmail.com>
2021-12-16 15:40:11 -0400

    base clients state

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import cartReducer from './features/cart/cartSlice'
 import productsReducer from './features/products/productsSlice'
+import clientsReducer from './features/clients/clientsSlice'
 
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
     products: productsReducer,
+    clients: clientsReducer,
   },
 })
