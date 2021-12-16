f7662ec3ea9d118587fc044a536cf36b3f959d10
Joseph Salen <jsalen.91@gmail.com>
2021-12-16 15:38:43 -0400

    base clients state

---
+++ src/features/clients/clientsSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  clients: [],
+}
+
+export const clientsSlice = createSlice({
+  name: 'clients',
+  initialState,
+  reducers: {
+    addClient: (state, action) => {
+      state.clients.push(action.payload)
+    },
+  },
+})
+
+export const { addClient } = clientsSlice.actions
+
+export default clientsSlice.reducer
