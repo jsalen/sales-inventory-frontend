b37ee106274e2ac966f12c9d22f04b0958f9e7ee
Joseph Salen <jsalen.91@gmail.com>
2021-12-18 13:12:01 -0400

    clear cart after page dismount

--- src/Pages/Sales/index.js
+++ src/Pages/Sales/index.js
@@ -1,15 +1,21 @@
-import { useState } from 'react'
-import { useSelector } from 'react-redux'
+import { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import { Modal } from '../../components/Modal'
 import { SalesModal } from '../../components/SalesModal'
 import { ProductPanel } from '../../containers/ProductPanel'
 import { SalesPanel } from '../../containers/SalesPanel'
+import { clearCart } from '../../features/cart/cartSlice'
 
 import { Container } from './styles'
 
 export const Sales = () => {
   const [openModal, setOpenModal] = useState(false)
   const products = useSelector((state) => state.cart.products)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    return () => dispatch(clearCart())
+  }, [])
 
   return (
     <Container products={products.length > 0}>
