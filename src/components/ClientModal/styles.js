0e6dc69ac32793c50542f5d0fcaa93b02c78ef31
Joseph Salen <jsalen.91@gmail.com>
2021-12-21 15:57:21 -0400

    fix grid details

--- src/components/ClientModal/styles.js
+++ src/components/ClientModal/styles.js
@@ -2,18 +2,23 @@ import styled from 'styled-components'
 
 export const ModalContainer = styled.article`
   width: 320px;
-  height: auto;
   padding: 16px;
   display: grid;
-  align-items: center;
-  grid-template-rows: 30px 1fr 45px;
+  grid-template-rows: 38px 1fr 45px;
   grid-gap: 16px;
   background: #f8f7ff;
-  text-align: center;
+  border-radius: 4px;
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.4);
 
   & h1 {
     font-size: 18px;
     font-weight: 700;
+    text-align: center;
+    border-bottom: 1px solid #cecece;
+  }
+
+  & p {
+    text-align: center;
   }
 
   & footer {
@@ -29,4 +34,8 @@ export const ModalButton = styled.button`
   background: ${(props) => (props.primary ? '#f8f7ff' : '#9381ff')};
   border: ${(props) => (props.primary ? '1px solid #9381ff' : '0')};
   cursor: pointer;
+
+  &:hover {
+    background: ${(props) => (props.primary ? '#f8f7ff' : '#7365c8')};
+  }
 `
