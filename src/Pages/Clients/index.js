3913cc867103098097494f88dffc26e9acc45873
Joseph Salen <jsalen.91@gmail.com>
2021-12-26 14:12:40 -0400

    implement create client form

--- src/Pages/Clients/index.js
+++ src/Pages/Clients/index.js
@@ -1,5 +1,8 @@
+import { useState } from 'react'
 import { useSelector } from 'react-redux'
+import { ClientCreateForm } from '../../components/ClientCreateForm'
 import { FilterBoard } from '../../components/FilterBoard'
+import { Modal } from '../../components/Modal'
 import { ClientsTable } from '../../containers/ClientsTable'
 import useFilterClients from '../../hooks/useFilterClients'
 
@@ -8,6 +11,7 @@ import { MdOutlineAdd } from 'react-icons/md'
 import { Container, Footer, Tooltip } from './styles'
 
 export const Clients = () => {
+  const [createClientModal, setCreateClientModal] = useState(false)
   const clients = useSelector((state) => state.clients.clients)
   const {
     query,
@@ -15,22 +19,34 @@ export const Clients = () => {
     filteredData: filteredClients,
   } = useFilterClients(clients)
 
-  return (
-    <Container>
-      <FilterBoard
-        title='Clientes'
-        placeHolder='Cliente...'
-        query={query}
-        setQuery={setQuery}
-      />
-      <ClientsTable clients={filteredClients} />
+  const handleModal = () => {
+    setCreateClientModal((prev) => !prev)
+  }
 
-      <Footer>
-        <button>
-          <Tooltip>Crear Cliente</Tooltip>
-          <MdOutlineAdd size='48' />
-        </button>
-      </Footer>
-    </Container>
+  return (
+    <>
+      <Container>
+        <FilterBoard
+          title='Clientes'
+          placeHolder='Cliente...'
+          query={query}
+          setQuery={setQuery}
+        />
+        <ClientsTable clients={filteredClients} />
+
+        <Footer>
+          <button onClick={handleModal}>
+            <Tooltip>Crear Cliente</Tooltip>
+            <MdOutlineAdd size='48' />
+          </button>
+        </Footer>
+      </Container>
+
+      {createClientModal && (
+        <Modal>
+          <ClientCreateForm handleModal={handleModal} />
+        </Modal>
+      )}
+    </>
   )
 }
