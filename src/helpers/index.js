89d09ebb4989b2247e3cbd4ec842b30b03e01ff8
Joseph Salen <jsalen.91@gmail.com>
2022-01-07 20:00:12 -0400

    create getTotal function

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,3 +1,4 @@
+import { useSelector } from 'react-redux'
 import dateFormat from 'dateformat'
 
 export const formatDate = (date) => {
@@ -5,4 +6,12 @@ export const formatDate = (date) => {
   const time = dateFormat(date, 'h:MM TT')
 
   return [day, time]
+}
+
+export const getTotal = (payment) => {
+  const { orders } = useSelector((state) => state.orders)
+  const sortedOrders = orders.filter((order) => order.payment === payment)
+  const total = sortedOrders.reduce((acc, order) => acc + order.total, 0)
+
+  return total
 }
