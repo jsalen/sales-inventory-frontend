f27240e57df729d83c5e8fe9653e983c08329af1
Joseph Salen <jsalen.91@gmail.com>
2022-01-08 21:16:01 -0400

    change entry point and filename of output webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,65 +2,61 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 
-module.exports = (env, args) => {
-  const { mode } = args
-  const isProduction = mode === 'production'
-
-  return {
-    entry: './src/index.js',
-    output: {
-      path: path.resolve(__dirname, 'build'),
-      filename: isProduction ? '[name].[contenthash].js' : 'main.js',
-      publicPath: '/',
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'main.js',
+    publicPath: '/',
+  },
+  devServer: {
+    port: 3000,
+    client: {
+      overlay: {
+        errors: true,
+        warnings: false,
+      },
     },
-    devServer: {
-      port: 3000,
-      client: {
-        overlay: {
-          errors: true,
-          warnings: false,
+    compress: true,
+    historyApiFallback: true,
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              '@babel/preset-react',
+              {
+                runtime: 'automatic',
+              },
+            ],
+          ],
         },
       },
-      compress: true,
-      historyApiFallback: true,
-    },
-    devtool: 'source-map',
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              [
-                '@babel/preset-react',
-                {
-                  runtime: 'automatic',
-                },
-              ],
-            ],
-          },
-        },
-        {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader'],
-        },
-        {
-          test: /\.png/,
-          type: 'asset/resource',
-        },
-      ],
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './public/index.html',
-      }),
-      new CopyPlugin({
-        patterns: [
-          { from: 'public/manifest.json', to: '' },
-          { from: 'public/assets/icon.png', to: 'assets' },
-        ],
-      }),
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.png/,
+        type: 'asset/resource',
+      },
     ],
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'public/manifest.json', to: '' },
+        { from: 'public/assets/icon.png', to: 'assets' },
+      ],
+    }),
+  ],
 }
